f154bf000005ecfa058dc31093d896e038a2c73c
ajha-digi <ajha@digisprint.com>
2022-09-01 18:31:18 +0530

    Prettified app

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
-import express from "express"
-import bodyParser from "body-parser"
-const app = express()
-app.use(bodyParser.json())
+import express from "express";
+import bodyParser from "body-parser";
+const app = express();
+app.use(bodyParser.json());
 app.get("/", (req, res) => {
-  res.send("Hello Babel")
-})
+  res.send("Hello Babel");
+});
 app.listen(4000, () => {
-  console.log(`app is listening to port 4000`)
-})
+  console.log(`app is listening to port 4000`);
+});
 
 // https://cloudnweb.dev/2019/07/configuring-babel-for-node-js-express-server/
