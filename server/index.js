6a7ab5870b0f5e45757989654ff65d77a0f7564d
ajha-digi <ajha@digisprint.com>
2022-09-01 17:30:01 +0530

    Basic es6 express server set up.

---
+++ server/index.js
@@ -0,0 +1,10 @@
+import express from "express"
+import bodyParser from "body-parser"
+const app = express()
+app.use(bodyParser.json())
+app.get("/", (req, res) => {
+  res.send("Hello Babel")
+})
+app.listen(4000, () => {
+  console.log(`app is listening to port 4000`)
+})
